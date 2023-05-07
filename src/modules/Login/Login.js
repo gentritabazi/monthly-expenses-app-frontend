9955d78132a964aad6f924657c3165f88bcec467
Gentrit Abazi <gentritabazi01@gmail.com>
2023-05-07 22:41:16 +0200

    Login Register Links

--- src/modules/Login/Login.js
+++ src/modules/Login/Login.js
@@ -1,5 +1,6 @@
 import GuestLayout from "@/layouts/Guest";
 import Api from "@/utils/api";
+import AppConfig from "@/config/app";
 
 export default {
   name: "Login",
@@ -17,6 +18,11 @@ export default {
 
   methods: {
     async login() {
+      if (AppConfig.DEMO_MODE) {
+        this.$router.push({ name: "dashboard" });
+        return;
+      }
+
       const response = await Api.post("/auth/login", { email: this.email, password: this.password });
 
       this.$store.commit("LOGGED_USER", response.data);
--- src/modules/Register/Register.js
+++ src/modules/Register/Register.js
@@ -1,8 +1,9 @@
 import GuestLayout from "@/layouts/Guest";
 import Api from "@/utils/api";
+import AppConfig from "@/config/app";
 
 export default {
-  name: "Login",
+  name: "Register",
 
   data() {
     return {
@@ -17,7 +18,12 @@ export default {
   },
 
   methods: {
-    login() {
+    register() {
+      if (AppConfig.DEMO_MODE) {
+        this.$router.push({ name: "dashboard" });
+        return;
+      }
+
       Api.post("/auth/register", { name: this.name, email: this.email, password: this.password });
 
       self.$router.go({ name: "login" });
